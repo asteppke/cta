56c9bc2d96d6f3ffd026f0cc1348db4c9158221b
Roger Biffiger <roger.biffiger@psi.ch>
2019-04-04 10:36:08 +0200

    Added conda packaging

---
+++ lib/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cta_lib',
+    version='1.0.0',
+    packages=['cta_lib'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='python library to interact with cta app on IOC',
+    long_description='',
+    requires=['pyepics', 'numpy']
+)
